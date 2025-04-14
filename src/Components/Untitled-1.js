b697ebbe94838bd0edc1bbcb1ac311c5754ab21b
Dennis Sarasibar <dens90@hotmail.it>
2025-04-14 17:51:07 +0200

    delete file test js file

---
+++ src/Components/Untitled-1.js
@@ -0,0 +1,49 @@
+
+  
+  export const dataNav= [
+    {
+      title: "Ricerca",
+      labels: [
+        "Ricerca Officina",
+        "Ricerca per Targa",
+        "Ricerca per Servizio",
+        "Officine Vicine",
+        "Mappa Officine",
+      ],
+    },
+    {
+      title: "servizi",
+      labels: [
+        "Servizi Offerti",
+        "Meccanico",
+        "Carrozzeria",
+        "Gommista",
+        "Elettrauto",
+        "Soccorso Stradale",
+      ],
+    },
+  
+    {
+      title: "Informazioni",
+      labels: [
+        "Informazioni Utili",
+        "Come funziona la ricerca",
+        "Filtri di Ricerca",
+        "Visualizzazione Mappa",
+        "Contatti Officine",
+      ],
+    },
+    {
+      title: "Area",
+      labels: [
+        "Area Utente",
+        "Officine Convenzionate",
+        "Le mie Ricerche",
+        "Assistenza",
+        "Segnala un'Officina",
+      ],
+    },
+  ];
+  
+
+  const filterItems = dataNav.map(item => console.log(item))
